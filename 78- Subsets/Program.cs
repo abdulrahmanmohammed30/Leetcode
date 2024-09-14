ef530a57d0e95d9196e48cbd64f7b4b234da4dba
Abdulrahman <abdoelswasy286@gmail.com>
2024-09-14 23:36:43 +0300

    Create Leetcode Problems Tracker

---
+++ 78- Subsets/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _78__Subsets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] nums = new int[] { 1, 2, 3,4 };
+           var res= Subsets(nums);
+           foreach(var i in res)
+            {
+                foreach(var j in i)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+       static public IList<IList<int>> Subsets(int[] nums)
+        {
+            var arr = new List<IList<int>>() { new List<int>() };
+            int sz = nums.Length;
+
+            for (int i = 0; i < sz; i++)
+            {
+                arr.Add(new List<int> { nums[i] });
+                GetCurrentElementSets(nums, i + 1, new List<int> { nums[i] });
+            }
+            return arr;
+
+
+            void GetCurrentElementSets(int[] nums, int startIndex, List<int> sets)
+            {
+                if (startIndex == sz)
+                    return;
+
+                for (int j = startIndex; j < sz; j++)
+                {
+                    sets.Add(nums[j]);
+                    arr.Add(new List<int>(sets));
+                    GetCurrentElementSets(nums, j + 1, sets);
+                    sets.Remove(nums[j]);
+                }
+            }
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var arr= new List<IList<int>>() { new List<int>()};
+            int sz = nums.Length;
+
+            for (int i = 0; i < sz; i++)
+            {
+                arr.Add(new List<int> {nums[i]});
+                GetCurrentElementSets(nums, i+1, new List<int> { nums[i] });
+            }
+            return arr; 
+
+
+            void GetCurrentElementSets(int[] nums, int index, List<int>sets) {
+                if (index == sz)
+                    return;
+
+                for(int j=index;index<sz;j++)
+                {
+                    sets.Add(nums[j]);
+                    arr.Add(new List<int>(sets));
+                    GetCurrentElementSets(nums, j + 1, sets);
+                    sets.Remove(nums[j]);
+                }
+            }
+        }
+    }
+}
+
+
+//List<int>? temp = null;
+//for (var j = 0; j < sz; j++)
+//{
+//    arr.Add(new List<int>() { nums[i], nums[j] });
+//    if (temp != null)
+//    {
+//        temp.Add(nums[j]);
+//        arr.Add(new List<int>(temp));
+
+//    }
+//    else
+//        temp = new List<int> { nums[i], nums[j] };
+//}
