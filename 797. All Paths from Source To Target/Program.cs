6195c54aa576e9f35837199066a6cd2e839eb16a
Abdulrahman <abdoelswasy286@gmail.com>
2024-09-17 19:52:31 +0300

    problem 797

---
+++ 797. All Paths from Source To Target/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _797._All_Paths_from_Source_To_Target
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = new Solution().AllPathsSourceTarget([[4, 3, 1], [3, 2, 4], [3], [4], []]);
+            foreach(var i in res)
+            {
+                foreach(var j in i)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public class Solution
+    {
+
+        void Solver(int[][] graph, int index, int target, List<int> path, List<IList<int>> paths) {
+
+            Console.WriteLine(index);
+            if (index == target)
+            {
+                paths.Add(new List<int>(path));
+                return;
+            }
+
+            foreach(var i in graph[index])
+            {
+                path.Add(i);
+                Solver(graph, i, target, path, paths);
+                path.Remove(i);
+            }
+        }
+        public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+
+            var paths = new List<IList<int>>();
+            var path=new List<int>() {0};
+
+             Solver(graph, 0, graph.Count() - 1, path, paths);
+
+            //return new List<IList<int>> { new List<int>() };
+            return paths;
+        }
+    }
+}
