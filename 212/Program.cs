5e8db286f330dc0df78e3892fde216eb46b46569
Abdulrahman <abdoelswasy286@gmail.com>
2024-09-20 19:14:17 +0300

    project

---
+++ 212/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _212_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+
+
+        public Node BuildTrie(string[] words) {
+            Node root = new Node();
+            foreach(var word in words)
+            {
+                Node node = root;
+                foreach(var ch in word)
+                {
+                    if (node.children[ch-'a']==null)
+                    {
+                        node.children[ch - 'a'] = new Node();
+                    }
+                    node = node.children[ch-'a']; 
+                }
+                node.word = word;
+            }
+            return root;
+        }
+        
+        public class Node
+        {
+            public Node[] children=new Node[26];
+            public string? word;
+        }
+    }
+}
