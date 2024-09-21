13693060645b4077e45da952c4cfb7d169c0f468
Abdulrahman <abdoelswasy286@gmail.com>
2024-09-21 21:50:29 +0300

    project

--- 212/Program.cs
+++ 212/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _212_
+﻿using System.Diagnostics.Tracing;
+
+namespace _212_
 {
     internal class Program
     {
@@ -10,6 +12,55 @@
     public class Solution
     {
 
+        public IList<string> FindWords(char[][] board, string[] words)
+        {
+            var res=new HashSet<string>();
+            var root = BuildTrie(words);
+
+            int mr = board.Length;
+            int mc = board[0].Length;
+
+            for (int r = 0; r < mr; r++)
+            {
+                for (int c = 0; c < mc; c++)
+                {
+                    DFS(board, r, c, root, res);
+                }
+            }
+
+            return res.ToList();
+        }
+
+        void DFS(char[][] board, int r, int c,Node node, HashSet<string> res)
+        {
+            
+            char ch = board[r][c];
+            if (ch == '#' || node.children[ch - 'a'] == null)
+                return;
+
+            node = node.children[ch - 'a'];
+            if (node.word != null)
+                res.Add(node.word);
+
+
+            board[r][c] = '#';  
+            int[] dirr = { -1, 1, 0, 0 };
+            int[] dirc = { 0, 0, -1, 1 };
+
+
+            for (int i=0;i<4;i++)
+            {
+                int nr = r+dirr[i];
+                int nc = c + dirc[i];
+
+
+                if (nr >= 0 && nr < board.Length && nc >= 0 && nc < board[0].Length)
+                {
+                    DFS(board, nr, nc, node, res);
+                }
+            }
+            board[r][c] = ch; 
+        }
 
         public Node BuildTrie(string[] words) {
             Node root = new Node();
---
+++ 648/Program.cs
@@ -0,0 +1,82 @@
+﻿using static _648.Solution;
+using System.Text;
+using System.Linq;
+
+namespace _648
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution()
+                .ReplaceWords(["cat", "bat", "rat"], "the cattle s was rattled by the battery"));
+        }
+    }
+    public class Solution
+    {
+        public string ReplaceWords(IList<string> dictionary, string sentence)
+        {
+            var root = BuildTrie(dictionary);
+
+            var words = sentence.Split(" ");
+            var res = new List<string>();
+            foreach (var word in words)
+            {
+                DFS(root, word, 0, res, word.Length);
+            }
+            return string.Join(" ", res);
+        }
+
+        public void DFS(Node node, string word, int index, List<string> res, int size)
+        {
+
+            if (index == size)
+            {
+                res.Add(word);
+                return;
+            }
+
+            char ch = word[index];
+            if (node.children[ch - 'a'] == null)
+            {
+                res.Add(word);
+                return;
+            }
+
+
+            node = node.children[ch - 'a'];
+            if (node.word != null)
+            {
+                res.Add(node.word);
+                return;
+            }
+
+           DFS(node, word, index+1, res);
+        }
+
+        public Node BuildTrie(IList<string> words)
+        {
+            Node root = new Node();
+            foreach (var word in words)
+            {
+                Node node = root;
+                foreach (var ch in word)
+                {
+                    if (node.children[ch - 'a'] == null)
+                    {
+                        node.children[ch - 'a'] = new Node();
+                    }
+                    node = node.children[ch - 'a'];
+                }
+                node.word = word;
+            }
+            return root;
+        }
+
+        public class Node
+        {
+            public Node[] children = new Node[26];
+            public string? word;
+        }
+    }
+}
