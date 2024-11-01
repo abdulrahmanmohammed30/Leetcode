725c3429fcf700e75c63a47a32c37da65af1a555
Abdulrahman <abdoelswasy286@gmail.com>
2024-11-01 23:14:30 +0200

    309. Best Time to Buy and Sell Stock with Cooldown/

---
+++ Backtracking/309. Best Time to Buy and Sell Stock with Cooldown/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+
+        int n = prices.Length;
+        var cache = new int[n, 2];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 2; j++) cache[i, j] = -1;
+
+        return Solver(0, 0);
+
+        int Solver(int i, int holding)
+        {
+            if (i >= n) return 0;
+
+            if (cache[i, holding] != -1) return cache[i, holding];
+            // don't sell or buy. Just Leave. 
+            cache[i, holding] = Solver(i + 1, holding);
+
+            // you can buy, coming from a Cooldown state
+            if (holding == 0)
+                cache[i, holding] = Math.Max(cache[i, holding], -1 * prices[i] + Solver(i + 1, 1));
+            // you can sell, holding a transaction
+            else
+                cache[i, holding] = Math.Max(cache[i, holding], prices[i] + Solver(i + 2, 0));
+
+            return cache[i, holding];
+        }
+    }
+}
+
