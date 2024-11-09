e91e6a48953d9e14d00eecc96d7ff5fdffd41d0f
Abdulrahman <abdoelswasy286@gmail.com>
2024-11-09 23:01:26 +0200

    optimize 343. Integer Break/Program.cs

--- Dynamic Programming/343. Integer Break/Program.cs
+++ Dynamic Programming/343. Integer Break/Program.cs
@@ -35,39 +35,75 @@
 //     }
 // }
 
-// optimized version 
+// Some optimizations  
+// public class Solution
+// {
+//     public int IntegerBreak(int n)
+//     {
+
+//         var visited = new HashSet<(int, int)>();
+//         int res = 0;
+//         Solver(0, 1);
+//         return res;
+
+//         void Solver(int sum, int product)
+//         {
+
+//             if (sum == n)
+//             {
+//                 res = Math.Max(res, product);
+//                 return;
+//             }
+
+//             if (visited.Contains((sum, product))) return;
+
+
+//             visited.Add((sum, product));
+
+//             for (int i = 1; i < n; i++)
+//             {
+//                 if (i + sum <= n)
+//                 {
+//                     Solver(i + sum, i * product);
+//                 }
+
+//             }
+//         }
+//     }
+// }
+
+// optimized soluion
 public class Solution
 {
     public int IntegerBreak(int n)
     {
-
-        var visited = new HashSet<(int, int)>();
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        var visited = new int [n + 1];
         int res = 0;
-        Solver(0, 1);
+        Solver(n, 1);
         return res;
 
         void Solver(int sum, int product)
         {
-
-            if (sum == n)
+            if (sum < 0) return;
+            
+            if (sum == 0)
             {
                 res = Math.Max(res, product);
                 return;
             }
+            
+            if (visited[sum] >= product) return; 
 
-            if (visited.Contains((sum, product))) return;
 
+            visited[sum]=product;
 
-            visited.Add((sum, product));
-
-            for (int i = 1; i < n; i++)
-            {
-                if (i + sum <= n)
-                {
-                    Solver(i + sum, i * product);
-                }
-
-            }
+            Solver(sum - 2, product * 2);
+            Solver(sum - 3, product * 3);
         }
     }
 }
+
+
