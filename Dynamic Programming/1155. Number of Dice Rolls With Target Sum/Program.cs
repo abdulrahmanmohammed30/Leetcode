c67eaf8949f856c8a182b5d852172a40bbada4c3
Abdulrahman <abdoelswasy286@gmail.com>
2024-11-20 16:38:07 +0200

    1155. Number of Dice Rolls With Target Sum

---
+++ Dynamic Programming/1155. Number of Dice Rolls With Target Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int NumRollsToTarget(int n, int k, int target)
+    {
+
+        var dp = new int[target + 1, n + 1];
+        for (int i = 0; i < target + 1; i++)
+            for (int j = 0; j < n + 1; j++)
+                dp[i, j] = -1;
+
+        return Solver(target, n);
+        int Solver(int r, int n)
+        {
+            if (r == 0 && n == 0) return 1;
+            if (n == 0) return 0;
+            if (dp[r, n] != -1) return dp[r, n];
+
+            int count = 0;
+            for (int i = 1; i <= k; i++)
+            {
+                if (r >= i) count = (int)((count + (int)(Solver(r - i, n - 1) % (Math.Pow(10, 9) + 7))) % (Math.Pow(10, 9) + 7));
+
+            }
+            dp[r, n] = count;
+            return count;
+        }
+    }
+}
