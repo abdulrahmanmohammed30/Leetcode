bad2b63e376a68e19832bef64c4c05d95f9274d1
Abdulrahman <abdoelswasy286@gmail.com>
2024-11-21 23:47:59 +0200

    91. Decode Ways

---
+++ Dynamic Programming/91. Decode Ways/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0') return 0;
+
+        var m = new int[s.Length];
+        Array.Fill(m, -1);
+
+        int res = Solver(s.Length - 1);
+        return res < 0 ? 0 : res;
+
+        int Solver(int i)
+        {
+            if (i <= 0) return 1;
+            if (m[i] != -1) return m[i];
+
+            int ans = 0;
+            int num = (s[i - 1] - '0') * 10 + (s[i] - '0');
+            if (s[i] == '0')
+            {
+                if (s[i - 1] == '0' || num > 26)
+                {
+                    m[i] = -500;
+                    return m[i];
+                }
+                ans = Solver(i - 2);
+            }
+            else
+            {
+                ans = Solver(i - 1);
+                if (ans == -500)
+                {
+                    m[i] = -500;
+                    return m[i];
+                }
+                if (s[i - 1] != '0' && num <= 26) ans += Solver(i - 2);
+            }
+            m[i] = ans;
+            return ans;
+        }
+    }
+}
