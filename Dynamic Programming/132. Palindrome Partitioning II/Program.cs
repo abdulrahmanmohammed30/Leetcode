9ab6f37196ece8f561383d5a7f6bd5de3d1bd7b1
Abdulrahman <abdoelswasy286@gmail.com>
2024-12-19 21:13:31 +0200

    132. Palindrome Partitioning II

---
+++ Dynamic Programming/132. Palindrome Partitioning II/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+
+    public int MinCut(string s)
+    {
+        int n = s.Length;
+
+        var db = new int?[n];
+        return Solver(0);
+        bool IsPalindrome(int start, int end)
+        {
+            if (start >= end)
+                return true;
+
+            if (s[start] != s[end])
+                return false;
+
+            return IsPalindrome(start + 1, end - 1);
+        }
+
+        int Solver(int start)
+        {
+            if (start >= n || IsPalindrome(start, n - 1))
+                return 0;
+
+            if (db[start].HasValue)
+                return db[start]!.Value;
+
+            int count = int.MaxValue;
+
+            for (int end = start; end < n - 1; end++)
+            {
+                if (IsPalindrome(start, end))
+                {
+                    count = Math.Min(count, 1 + Solver(end + 1));
+                }
+            }
+            db[start] = count;
+            return count;
+        }
+
+    }
+}
