37a16871539285ea36adea72be261818307a91a3
Abdulrahman <abdoelswasy286@gmail.com>
2024-12-29 12:47:53 +0200

    1049. Last Stone Weight II

--- Backtracking/1049. Last Stone Weight II/Program.cs
+++ Backtracking/1049. Last Stone Weight II/Program.cs
@@ -4,19 +4,22 @@
     {
         int n = stones.Length;
         int total = stones.Sum();
-        var db = new int[n, total + 1];
+        int halfTotal = total / 2;
+
+        var db = new int[n, halfTotal + 1];
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < total + 1; j++)
+            for (int j = 0; j <= halfTotal; j++)
                 db[i, j] = -1;
 
-
         return Solver(0, 0);
+
         int Solver(int idx, int sum)
         {
+            if (sum > halfTotal) return int.MaxValue;
             if (idx == n)
             {
                 int sum2 = total - sum;
-                return Math.Abs(sum - sum2);
+                return sum2 - sum;
             }
             if (db[idx, sum] != -1) return db[idx, sum];
 
@@ -27,6 +30,37 @@
         }
     }
 }
+
+
+// public class Solution
+// {
+//     public int LastStoneWeightII(int[] stones)
+//     {
+//         int n = stones.Length;
+//         int total = stones.Sum();
+//         var db = new int[n, total + 1];
+//         for (int i = 0; i < n; i++)
+//             for (int j = 0; j < total + 1; j++)
+//                 db[i, j] = -1;
+
+
+//         return Solver(0, 0);
+//         int Solver(int idx, int sum)
+//         {
+//             if (idx == n)
+//             {
+//                 int sum2 = total - sum;
+//                 return Math.Abs(sum - sum2);
+//             }
+//             if (db[idx, sum] != -1) return db[idx, sum];
+
+//             int fg = Solver(idx + 1, sum + stones[idx]);
+//             int sg = Solver(idx + 1, sum);
+//             db[idx, sum] = Math.Min(fg, sg);
+//             return db[idx, sum];
+//         }
+//     }
+// }
 
 // thinking: 1 2 3
 // writing code  1
