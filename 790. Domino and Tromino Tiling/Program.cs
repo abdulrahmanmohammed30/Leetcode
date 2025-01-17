208c613e14a3dfe76241a6150d71d69b5d815bf2
Abdulrahman <abdoelswasy286@gmail.com>
2025-01-17 21:25:23 +0200

    790. Domino and Tromino Tiling

---
+++ 790. Domino and Tromino Tiling/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Solution
+{
+    public int NumTilings(int n)
+    {
+        var d = new int?[n + 1, 4];
+        var MODD = 1000000007;
+
+        return Solver(0, true, true);
+        int Solver(int i, bool t1, bool t2)
+        {
+            if (i == n)
+                return 1;
+
+            int state = 0;
+            if (t1) state |= 1;
+            if (t2) state |= 2;
+
+            if (d[i, state].HasValue) return d[i, state]!.Value;
+            int count = 0;
+            bool t3 = i + 1 < n, t4 = i + 1 < n;
+
+            // put one 2 * 1 vertically 
+            if (t1 && t2) count = (count + Solver(i + 1, true, true)) % MODD;
+
+            // put two 2 * 1 horizontally 
+            if (t1 && t2 & t3 & t4) count = (count + Solver(i + 1, false, false)) % MODD;
+
+            // put one L 
+            if (t1 && t2 && t3) count = (count + Solver(i + 1, false, true)) % MODD;
+
+            // put one L 
+            if (t1 && t2 && t4) count = (count + Solver(i + 1, true, false)) % MODD;
+
+            // use L 
+            if (t1 && !t2 && t3 && t4) count = (count + Solver(i + 1, false, false)) % MODD;
+
+            // use a vertical 2 * 1 
+            if (t1 && !t2 && t3) count = (count + Solver(i + 1, false, true)) % MODD;
+
+            // use an L 
+            if (!t1 && t2 && t3 && t4) count = (count + Solver(i + 1, false, false)) % MODD;
+
+            // use a vertical 2 * 1 
+            if (!t1 && t2 && t4) count = (count + Solver(i + 1, true, false)) % MODD;
+
+            if (!t1 && !t2) count = (count + Solver(i + 1, true, true)) % MODD;
+
+            d[i, state] = count % MODD;
+
+            return count % MODD;
+        }
+    }
+}
+
