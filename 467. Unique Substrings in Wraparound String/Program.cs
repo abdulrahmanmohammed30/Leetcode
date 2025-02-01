d5aa67dbcc9c2712203ee081d323c9f9aed9cb9b
Abdulrahman <abdoelswasy286@gmail.com>
2025-02-01 22:58:17 +0200

    467. Unique Substrings in Wraparound String

---
+++ 467. Unique Substrings in Wraparound String/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int FindSubstringInWraproundString(string p)
+    {
+        Func<char, char, bool> IsNext = (a, b) =>
+          {
+              if (a == 'z') return b == 'a';
+              return a + 1 == b;
+          };
+
+        int maxLength = 1;
+        int[] arr = new int[26];
+
+
+        for (int i = 0; i < p.Length; i++)
+        {
+            int idx = p[i] - 'a';
+            arr[idx] = Math.Max(maxLength, arr[idx]);
+
+            if (i + 1 < p.Length && IsNext(p[i], p[i + 1])) maxLength++;
+            else maxLength = 1;
+        }
+
+        int res = 0;
+        for (int i = 0; i < 26; i++) res += arr[i];
+        return res;
+    }
+}
